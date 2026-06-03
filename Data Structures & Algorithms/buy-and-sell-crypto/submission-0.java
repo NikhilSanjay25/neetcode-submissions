01c19adde3958a8ceed31391a56ae747ce2c861f
Nikhil Sanjay <152269472+NikhilSanjay25@users.noreply.github.com>
2026-06-03 10:14:01 +0000

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        if(prices.length==1){
+            return 0;
+        }
+        int max = 0;
+        int prev;
+        while(left<prices.length && right <prices.length){
+            max = Math.max(max,prices[right]-prices[left]);
+            if(prices[right]<prices[left]){
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+}
